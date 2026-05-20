530fb8b3581559447508282f5aa6ff670c92b219
Renan <renandiutida@gmail.com>
2026-05-20 00:29:41 -0300

    feat(Controllers): Adiciona o scaffold dos 4 controllers (ConcessionariasController, ClientesController, ContratacoesController e FinanciamentosController)

---
+++ CP3ConcessionariaAPI/Controllers/ConcessionariasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CP3ConcessionariaAPI.Data;
+using CP3ConcessionariaAPI.Models;
+
+namespace CP3ConcessionariaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcessionariasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ConcessionariasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Concessionarias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Concessionaria>>> GetConcessionarias()
+        {
+            return await _context.Concessionarias.ToListAsync();
+        }
+
+        // GET: api/Concessionarias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Concessionaria>> GetConcessionaria(int id)
+        {
+            var concessionaria = await _context.Concessionarias.FindAsync(id);
+
+            if (concessionaria == null)
+            {
+                return NotFound();
+            }
+
+            return concessionaria;
+        }
+
+        // PUT: api/Concessionarias/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConcessionaria(int id, Concessionaria concessionaria)
+        {
+            if (id != concessionaria.IdConcessionaria)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(concessionaria).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConcessionariaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Concessionarias
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Concessionaria>> PostConcessionaria(Concessionaria concessionaria)
+        {
+            _context.Concessionarias.Add(concessionaria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConcessionaria", new { id = concessionaria.IdConcessionaria }, concessionaria);
+        }
+
+        // DELETE: api/Concessionarias/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConcessionaria(int id)
+        {
+            var concessionaria = await _context.Concessionarias.FindAsync(id);
+            if (concessionaria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Concessionarias.Remove(concessionaria);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ConcessionariaExists(int id)
+        {
+            return _context.Concessionarias.Any(e => e.IdConcessionaria == id);
+        }
+    }
+}
---
+++ CP3ConcessionariaAPI/Controllers/FinanciamentosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CP3ConcessionariaAPI.Data;
+using CP3ConcessionariaAPI.Models;
+
+namespace CP3ConcessionariaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinanciamentosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FinanciamentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Financiamentos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Financiamento>>> GetFinanciamentos()
+        {
+            return await _context.Financiamentos.ToListAsync();
+        }
+
+        // GET: api/Financiamentos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Financiamento>> GetFinanciamento(int id)
+        {
+            var financiamento = await _context.Financiamentos.FindAsync(id);
+
+            if (financiamento == null)
+            {
+                return NotFound();
+            }
+
+            return financiamento;
+        }
+
+        // PUT: api/Financiamentos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFinanciamento(int id, Financiamento financiamento)
+        {
+            if (id != financiamento.IdProduto)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(financiamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FinanciamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Financiamentos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Financiamento>> PostFinanciamento(Financiamento financiamento)
+        {
+            _context.Financiamentos.Add(financiamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFinanciamento", new { id = financiamento.IdProduto }, financiamento);
+        }
+
+        // DELETE: api/Financiamentos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFinanciamento(int id)
+        {
+            var financiamento = await _context.Financiamentos.FindAsync(id);
+            if (financiamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Financiamentos.Remove(financiamento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FinanciamentoExists(int id)
+        {
+            return _context.Financiamentos.Any(e => e.IdProduto == id);
+        }
+    }
+}
